6a9b7ecaeb1407fe6efab040cd73c64b169d014a
Eorium <travisgent312@gmail.com>
2020-06-24 14:43:28 -0600

    add login mockup and links to support ticket

--- support-ticket/src/App.js
+++ support-ticket/src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 import TicketReceived from "./TicketReceived";
 import Ticket from './Ticket';
+import Register from "./Login";
 import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
-        <Route exact path="/" component={Ticket} />
-        <Route path="/TicketReceived" component={TicketReceived} />
+        <Route exact path="/" component={Register} />
+        <Route exact path="/Ticket" component={Ticket} />
+        <Route exact path="/TicketReceived" component={TicketReceived} />
       </BrowserRouter>
     </div>
   );
